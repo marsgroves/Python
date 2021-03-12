2ca7b82dbe8fd65defabc5e97f1c0a937c1e509a
Mars Groves <ocmarsgroves@gmail.com>
2021-03-11 22:56:04 -0800

    Sample

--- scripts/sample.py
+++ scripts/sample.py
@@ -1,8 +1,7 @@
-# Samle Python scripts
+# Sample Python scripts
 
 Example 1: Add Two Numbers
 ```# This program adds two numbers
-
 num1 = 1.5
 num2 = 6.3
 
@@ -12,3 +11,5 @@ sum = num1 + num2
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 ```
+
+.
