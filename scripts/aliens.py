0ea7b114ee97c1a57db9becfbc242a8bba1269dd
Mars Groves <ocmarsgroves@gmail.com>
2021-04-03 22:35:00 -0700

    added code

--- scripts/aliens.py
+++ scripts/aliens.py
@@ -11,3 +11,10 @@ if not pygame.image.get_extended():
     raise SystemExit("Sorry, extended image module required")
 
 #game constants
+
+MAX_SHOTS      = 3      #most player bullets onscreen
+ALIEN_ODDS     = 17     #chances a new alien appears
+BOMB_ODDS      = 40    #chances a new bomb will drop
+ALIEN_RELOAD   = 12     #frames between new aliens
+SCREENRECT     = Rect(0, 0, 640, 480)
+SCORE          = 0
